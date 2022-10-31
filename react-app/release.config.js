6e9adba88e30fdc811a04efdddfdf9d8a6a0efed
Manuel Rodriguez: manrodri <manuel@manrodri.com>
2022-10-31 10:49:44 +0000

    fix: add .releaserc

--- react-app/release.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  branches: "main",
-  repositoryUrl: "https://github.com/manrodri-learning/github-actions-react",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
-  ]
-};
