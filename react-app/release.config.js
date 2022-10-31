5339d6fbba45af3d942dc1ed296d9cf97faf387e
Manuel Rodriguez: manrodri <manuel@manrodri.com>
2022-10-31 10:25:21 +0000

    format release.config.js

--- react-app/release.config.js
+++ react-app/release.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-	branches: "main",
-	repositoryUrl: "https://github.com/manrodri-learning/github-actions-react",
-	plugins: [
-		"@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", 
-		"@semantic-release/github"
-		]
-}
+  branches: "main",
+  repositoryUrl: "https://github.com/manrodri-learning/github-actions-react",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
